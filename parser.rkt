57799c03ac3f7342c854cac6941a25bf95ff2d0e
MaanasaYepuru <134438911+MaanasaYepuru@users.noreply.github.com>
2026-04-09 15:51:28 -0500

    parser.rkt

---
+++ parser.rkt
@@ -0,0 +1,30 @@
+#lang brag
+
+program : room*
+
+room : LPAREN "room" ID room-body RPAREN
+
+room-body : room-element*
+
+room-element : desc
+             | item
+             | monster
+             | exit
+             | power
+
+desc : LPAREN "desc" STRING RPAREN
+
+item : LPAREN "item" ID item-body RPAREN
+
+item-body : item-field*
+
+item-field : desc
+           | type
+
+type : LPAREN "type" ID RPAREN
+
+monster : LPAREN "monster" ID NUMBER RPAREN
+
+exit : LPAREN "exit" ID ID RPAREN
+
+power : LPAREN "power" NUMBER RPAREN
