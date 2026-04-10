ffa431c118a03b324fde1b7964d48e5d8d9df35d
rskutt6 <r.a.schutt@icloud.com>
2026-04-10 09:45:22 -0500

    updated expander

---
+++ main.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+;; import your components
+(require "tokenizer.rkt"
+         "parser.rkt"     ;; generated from your brag file
+         "expander.rkt")
+
+;; =========================
+;; Runs your full pipeline
+;; =========================
+
+(define (run-file path)
+  ;; open the input file
+  (define in (open-input-file path))
+
+  ;; run parser using your tokenizer
+  (define parse-tree
+    (parse path (make-tokenizer in)))
+
+  ;; OPTIONAL: print parse tree for debugging
+  ;; (pretty-print (syntax->datum parse-tree))
+
+  ;; run your expander
+  (expand-program parse-tree)
+
+  ;; close file
+  (close-input-port in))
+
+;; =========================
+;; Run automatically
+;; =========================
+
+(module+ main
+  (run-file "test1.txt"))
