92946826521fc840785209055b48a531ff6d2e74
Jojo <jojopbeard@gmail.com>
2026-04-17 12:34:08 -0500

    condensed main

--- main.rkt
+++ main.rkt
@@ -1,37 +1,11 @@
-#lang racket
+#lang br/quicklang
+(require "parser.rkt" "tokenizer.rkt")
 
-;; import your components
-(require "lexer.rkt"
-         "parser.rkt"     ;; generated from your brag file
-         "expander.rkt")
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port path)))
+  (strip-bindings
+   #`(module basic-parser-mod COMP360_FinalProject/expander
+       #,parse-tree)))
 
-;; =========================
-;; Runs your full pipeline
-;; =========================
-
-(define (run-file path)
-  ;; open the input file
-  (define in (open-input-file path))
-
-  ;; run parser using your tokenizer
-  (define parse-tree
-    (parse path (make-tokenizer in)))
-
-  ;; OPTIONAL: print parse tree for debugging
-  ;; (pretty-print (syntax->datum parse-tree))
-
-  ;; run your expander
-  (expand-program parse-tree)
-
-  ;; close file
-  (close-input-port in))
-
-;; =========================
-;; Run automatically
-;; =========================
-
-(module+ main
-  (displayln "--- test.txt ---")
-  (run-file "test.txt")
-  (displayln "--- test2.txt ---")
-  (run-file "test2.txt"))
+(module+ reader
+  (provide read-syntax))
