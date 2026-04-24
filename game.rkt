9c64286a5d17eb3dbfe879108d889290b1b8d30a
undak-26 <undak-26@rhodes.edu>
2026-04-24 12:34:15 -0500

    Update game.rkt

--- game.rkt
+++ game.rkt
@@ -3,5 +3,9 @@
 
 (define (play world)
   (displayln "Welcome to the dungeon!"))
+  (define rooms (game-rooms world))
+  (define start-room (hash-ref rooms 'wake))
+  (displayln (room-v-name start-room))
+  (displayln (room-v-desc start-room)))
 
 (provide play)
