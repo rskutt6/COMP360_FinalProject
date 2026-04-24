e64d9814f172fc0a1d4072e917c6b412194e7df6
undak-26 <undak-26@rhodes.edu>
2026-04-24 12:37:27 -0500

    Update game.rkt

--- game.rkt
+++ game.rkt
@@ -7,5 +7,7 @@
   (define start-room (hash-ref rooms 'wake))
   (displayln (room-v-name start-room))
   (displayln (room-v-desc start-room)))
+  (for ([e (room-v-exits start-room)])
+    (printf "Exit: ~a\n" (exit-v-direction e))))
 
 (provide play)
